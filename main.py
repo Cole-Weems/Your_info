0ee077d7423e097b9413787f0099ee26deaa1271
Cole <coleaweems@gmail.com>
2026-06-29 17:00:58 -0400

    Setup function for entry fields

--- main.py
+++ main.py
@@ -1,26 +1,62 @@
 import tkinter as tk
 
-root = tk.Tk(screenName="Ava", baseName="Ava", className="Ava", useTk=1)
 
-label = tk.Label(root, text="Welcome to Ava, your local voice assistant")
+def entry_field(identifier, label, width):
+    entry = ((identifier), "_entry")
+    frame = ((identifier), "_frame")
+
+    entry = tk.Entry(frame)
+    entry.pack(side="left", fill="x", expand=True)
+
+    frame = tk.Frame(root)
+    frame.pack(fill="x", padx=10, pady=5)
+
+    tk.Label(
+        frame,
+        text=label,
+        width=width,
+        anchor="w"
+    ).pack(side="left")
+
+    entry = tk.Entry(frame)
+    entry.pack(side="left", fill="x", expand=True)
+
+    return entry
+
+
+root = tk.Tk()
+root.title("Your Info")
+
+label = tk.Label(root, text="A simple script to save your info to a pdf for printing")
 label.pack()
 
 
-label = tk.Label(root, text="You can use the below button to shutdown the assistant at any time")
+label = tk.Label(root, text="You can use the below button to export to a pdf")
 label.pack()
 
-button = tk.Button(root, text="Shutdown", width=25, command=root.destroy)
+button = tk.Button(root, text="Export", width=25, command=root.destroy)
 button.pack()
 
 
-tk.Label(root, text="First Name").grid(row=0, column=0)
-tk.Label(root, text="Last Name").grid(row=1, column=0)
+entry_field("first_name", "First name:", 12)
 
-entry1 = tk.Entry(root)
-entry2 = tk.Entry(root)
+entry_field("last_name", "Last name:", 12)
 
-entry1.grid(row=0, column=1)
-entry2.grid(row=1, column=1)
+label = tk.Label(root, text="Sex:")
+label.pack()
 
+lb = tk.Listbox(root)
+lb.insert(1, "Male")
+lb.insert(2, "Female")
+
+lb.pack()
+
+
+entry_field("height", "height:", 12)
+
+
+entry_field("country", "Country of residence:", 18)
+
+entry_field("age", "Age:", 12)
 
 root.mainloop()
